83b288e5c3c0b64c780062c51e4b0643568ef6c9
Myanh <90069985+MyanhTran@users.noreply.github.com>
2025-03-31 22:05:04 -0400

    Create main.py
    create constructor and dta processing

---
+++ main.py
@@ -0,0 +1,47 @@
+# step 1: download library 
+import torch 
+import numpy as np
+from torchvision import datasets
+import torchvision.transforms as transforms
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
+
+class main(object):
+    def __init__(self, **kwargs): 
+        #use **kwargs: to capture any keyword arguments (key, values) pairs not explitly defined in its parameter list
+        self.batchsize = kwargs.pop("batch_size",128)
+        # step2: load the data
+        # download, pre-processing + data augentament, split, create data loader
+         
+        
+        #transform setting
+        mytransform = transforms.Compose([
+                                        transforms.RandomHorizontalFlip(),
+                                        transforms.RandomVerticalFlip(),
+                                        transforms.RandomRotation(), 
+                                        #convert image to PIL-python image library 
+                                        transforms.ToTensor(),
+                                        transforms.Normalize([0.5,0.5,0.5], [0.5,0.5,0.5])
+                                        ])
+        #download
+        train = datasets.CIFAR10(train=True, download=True, transform=mytransform)
+        test = datasets.CIFAR10(train=False, download=True, transform=mytransform)
+
+        #split 
+        train_idx, valid_idx = train_test_split(train_size=0.8, test_size=0.2,shuffle=True)
+        train, val = Dataset(train_idx), Dataset(valid_idx)
+        train_loader = DataLoader(train,
+                                  batchsize = self.batchsize, shuffle = True)
+        val_loader = DataLoader(val, batch_size=self.batchsize)
+        test_loader = DataLoader(test, batchsize = self.batchsize)
+
+        classed = ['bird', 'cat', 'deer', 'dog']
+
+    
+        
+
+    def train(self):
+        pass
+
+    def evaluate(self, epoch):
+        pass
