dcc0c85879fe2cd3e9624dae65f8c6c08ed1c063
mtran324 <mtran324@gatech.edu>
2025-04-01 21:42:10 -0400

    create show_batch_img

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ import matplotlib.pyplot as plt
 class main(object):
     def __init__(self, **kwargs): 
         #use **kwargs: to capture any keyword arguments (key, values) pairs not explitly defined in its parameter list
-        self.batchsize = kwargs.pop("batch_size",16)
+        self.batchsize = kwargs.pop("batch_size", 16)
         # step2: load the data
         # download, pre-processing + data augentament, split, create data loader
          
         
         #transform setting
         mytransform = transforms.Compose([
+                                        transforms.Resize((256,256)), #images in Flower102 doesn't have same size
                                         transforms.RandomHorizontalFlip(),
                                         transforms.RandomVerticalFlip(),
                                         transforms.RandomRotation(degrees=10), 
@@ -27,27 +28,33 @@ class main(object):
                                         transforms.Normalize([0.5,0.5,0.5], [0.5,0.5,0.5])
                                         ])
         #download
-        train = datasets.CIFAR10(root='./data', train=True, download=True, transform=mytransform)
-        test = datasets.CIFAR10(root='./data', train=False, download=True, transform=mytransform)
+        train = datasets.Flowers102(root='./data', split='train', download=True, transform=mytransform)
+        val = datasets.Flowers102(root='./data', split='val', download=True, transform=mytransform)
+        test = datasets.Flowers102(root='./data', split='test', download=True, transform=mytransform)
 
-        #split 
-        train_idx, valid_idx = train_test_split(range(len(train)), test_size=0.2,shuffle=True)
-        
-        train_subset = Subset(train, train_idx)
-        val_subset = Subset(train, valid_idx)
 
-        self.train_loader = DataLoader(train_subset, batch_size=self.batchsize, shuffle = True)
-        self.val_loader = DataLoader(val_subset, batch_size=self.batchsize, shuffle=False)
+        self.train_loader = DataLoader(train, batch_size=self.batchsize, shuffle = True)
+        self.val_loader = DataLoader(val, batch_size=self.batchsize, shuffle=False)
         self.test_loader = DataLoader(test, batch_size=self.batchsize, shuffle=False)
 
-        classed = ['bird', 'cat', 'deer', 'dog']
+    def show_batch_img(self):
+        #get batch image from 1 single interation of data loader
+        data_iter = iter(self.train_loader)
+        images, labels = next(data_iter)
 
-    def show_img(self, img):
-        img = img *0.5 + 0.5 #we normalize image to tensor (C x H x W), now we need to convert it back (H W C)
-        img = np.transpose(img.numpy(), (1,2,0))
-        plt.imshow(img)
-        plt.axis('off')
-        plt.show()        
+        nrows = int(np.sqrt(self.batchsize))
+        ncols = int(np.ceil(self.batchsize / nrows))
+
+        #display the grid 
+        fig, axes = plt.subplots(nrows, ncols, figsize=(12,12))
+        axes = axes.flatten()
+
+        for img, label, ax in zip(images, labels, axes):
+            img = img.permute((1,2,0)).numpy()
+            ax.imshow(img)
+            ax.set_title(f"label: {label.item()}")
+            ax.axis('off')
+        plt.show()
 
     def train(self):
         pass
@@ -55,15 +62,5 @@ class main(object):
     def evaluate(self, epoch):
         pass
 
-#create an instance of a class 
 main_object = main()
-#access the train loader (this is why we use reference 'self' in main)
-train_loader = main_object.train_loader
-
-#get a batch of image 
-data_iter = iter(train_loader) #iter convert Iterable object (DataLoader) object to iterable
-images, labels = next(data_iter) #next give items in iterable
-
-#show batch of images 
-img_grid = make_grid(images, nrow=4)
-main_object.show_img(img_grid)
+main_object.show_batch_img()
